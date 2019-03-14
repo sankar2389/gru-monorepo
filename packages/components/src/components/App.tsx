c17e588a816cf3792b83e20aa7953f0f41187d05
0mkar <0mkar@localhost.localdomain>
2019-03-14 18:30:28 +0530

    load components from common workspace

--- packages/components/src/components/App.tsx
+++ packages/components/src/components/App.tsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import './App.css'
 
 export function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Buy/Sell on gru
+          </a>
+      </header>
     </div>
   )
 }
--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,15 +1,19 @@
-import { ComponentType } from 'react'
-import { AppRegistry } from 'react-native'
-
-import './index.css'
-
-import { App } from '@gru/components/src/components/App'
+import { ComponentType } from 'react';
+import { AppRegistry } from 'react-native';
+import './index.css';
+import { App } from '@gru/components/src/components/App';
+import * as serviceWorker from './serviceWorker';
 
 const render = (AppComponent: ComponentType) => {
-  AppRegistry.registerComponent('devhub', () => AppComponent)
-  AppRegistry.runApplication('devhub', {
+  AppRegistry.registerComponent('gruapp', () => AppComponent)
+  AppRegistry.runApplication('gruapp', {
     rootTag: document.getElementById('root'),
   })
 }
 
 render(App)
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
