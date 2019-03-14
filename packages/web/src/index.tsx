8793aa0a53556c40c5a6db2d9ddf669978652e69
0mkar <0mkar@localhost.localdomain>
2019-03-14 11:30:09 +0530

    configure monorepo from devhub

--- packages/web/src/index.tsx
+++ packages/web/src/index.tsx
@@ -1,11 +1,9 @@
-import { ComponentType } from 'react';
-import { AppRegistry } from 'react-native-web';
-import './index.css';
-import App from '@gru-monorepo/components/src/App';
-// import App from './App';
-import * as serviceWorke0r from './serviceWorker';
+import { ComponentType } from 'react'
+import { AppRegistry } from 'react-native'
 
-//ReactDOM.render(<App />, document.getElementById('root'));
+import './index.css'
+
+import { App } from '@gru/components/src/components/App'
 
 const render = (AppComponent: ComponentType) => {
   AppRegistry.registerComponent('devhub', () => AppComponent)
@@ -15,8 +13,3 @@ const render = (AppComponent: ComponentType) => {
 }
 
 render(App)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
