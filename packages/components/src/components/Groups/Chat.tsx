d0ff903c1d0e3ff149a8b5904bd1b044a39f3802
Subrata Biswas <subratabiswas.it@gmail.com>
2019-06-14 12:19:43 +0530

    clear chat page unuse code

--- packages/components/src/components/Groups/Chat.tsx
+++ packages/components/src/components/Groups/Chat.tsx
@@ -1,11 +1,10 @@
 import React, { Component } from "react";
 import { RouteComponentProps } from "react-router";
-import { IReduxState, IGroup, IAuth, IStrapiUser } from "../../types";
+import { IReduxState, IGroup } from "../../types";
 import { connect } from "react-redux";
-import { View, StyleSheet, AsyncStorage, Text, TouchableOpacity, Alert, Image, TextInput, ScrollView } from "react-native";
-import { getGroupsList, webSocketMiddlewareConnectOrJoin, webSocketDisconnect, webSocketConnect, onSendMessage, connected } from "../../actions";
+import { View, StyleSheet, AsyncStorage, Text, TouchableOpacity, Image, TextInput } from "react-native";
+import { getGroupsList, webSocketMiddlewareConnectOrJoin, webSocketDisconnect, webSocketConnect, onSendMessage } from "../../actions";
 import moment from "moment";
-import { any } from "prop-types";
 import PeopleChat from "./peopleChat"
 import GroupChat from "./groupChat"
 
@@ -319,11 +318,9 @@ class Chat extends Component<IProps, IState> {
 
                         <View style={styles.messageWriteView}>
                             <TextInput style={styles.writeMessageTextInput}
-                                //ref={component => this._root = component}
                                 autoFocus={true}
                                 editable={this.state.groupConnected ? true : false}
                                 placeholder={this.state.groupConnected ? "Write a reply..." : "Unable to establish connection to the messaging server. Please reload and try again."}
-                                //multiline={true}
                                 numberOfLines={4}
                                 value={this.state.replyText}
                                 onChangeText={(replyText) => this.onHandelChangeReplyInput(replyText)}
@@ -382,9 +379,7 @@ const styles = StyleSheet.create({
         padding: 20,
         width: "70%",
     },
-    customMessageView: {
-        marginLeft: 70
-    },
+
     flexLeftView: { flex: 1.8, height: "82vh" },
     textAndAddButtonView: { flexDirection: "row", alignItems: "center", justifyContent: "center" },
     addButton: { backgroundColor: "tomato", borderRadius: 50, height: 40, width: 40, paddingTop: 5 },
@@ -400,24 +395,14 @@ const styles = StyleSheet.create({
         backgroundColor: "#bfbfbf",
         borderRadius: 50,
     },
-    groupView: { height: "85%", overflowY: "scroll" },
     groupNameView: { flex: 1, marginLeft: 20, alignItems: "flex-start", },
     groupNameText: { flexWrap: "wrap", fontWeight: "900" },
     groupDateTime: { marginBottom: 10, color: "gray", fontSize: 12 },
-    groupListView: {
-        padding: 20, flexDirection: "row", justifyContent: "space-around",
-        borderColor: "#E5DFDF", borderWidth: 1,
-    },
 
     rightSideListView: {
         padding: 22, flexDirection: "row", justifyContent: "space-around",
         borderLeftColor: "#E5DFDF", borderLeftWidth: 1,
     },
-    selectedGroup: { backgroundColor: "#f0f0f0" },
-    memberLenght: {
-        backgroundColor: "tomato", borderRadius: 50, height: 25, width: 25, marginLeft: 5, justifyContent: "center"
-    },
-    memberLengthText: { color: "#ffffff" },
     rightSideView: { flex: 5, },
     messageView: {
         height: "70%", backgroundColor: "#f0f0f0", overflowY: "scroll",
